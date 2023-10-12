c480c4dc7bcdfd908aea4af5b9d0becce0557860
ren5602 <naziz5602@gmail.com>
2023-10-12 12:59:30 +0700

    modif

--- Pemilihan2Percobaan119.java
+++ Pemilihan2Percobaan119.java
@@ -7,6 +7,9 @@ public class Pemilihan2Percobaan119 {
     if ((tahun % 4) == 0){
         if ((tahun % 100) != 0)
             System.out.println("Tahun Kabisat");
+            else{
+                System.out.println("Bukan Tahun Kabisat");
+            }
     } else
         System.out.println("Tahun Bukan Kabisat");
     }
